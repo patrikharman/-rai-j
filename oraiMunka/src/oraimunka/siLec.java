184ac8fd1d60cf9245153392bbe5ca1b2b0a0271
patrikharman <147071748+patrikharman@users.noreply.github.com>
2023-11-19 16:36:37 +0100

    Add files via upload

---
+++ oraiMunka/src/oraimunka/siLec.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oraimunka;
+
+/**
+ *
+ * @author Patrik
+ */
+class siLec {
+
+    static String getNev() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static String getAr() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
